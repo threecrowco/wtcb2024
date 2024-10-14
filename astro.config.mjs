2f1fbc7a1e49727d4d8070a98e01da4b8596205f
Chris Canterbury <studio@threecrow.co>
2024-10-14 01:46:05 -0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,29 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import partytown from '@astrojs/partytown';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import simpleStackQuery from 'simple-stack-query';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [
+    partytown(),
+    tailwind(),
+    icon(),
+    sitemap(),
+    mdx(),
+    simpleStackQuery()
+  ]
 });
